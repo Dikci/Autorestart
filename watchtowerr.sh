622adf00131b27186fccb89825bbe6a6340f9717
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-03-27 16:05:21 +0300

    Update watchtowerr.sh

--- watchtowerr.sh
+++ watchtowerr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "[Unit]\nDescription=Monitoring Script\nAfter=network.target\n\n[Service]\nExecStart=/bin/bash /root/monitoring.sh\nRestart=always\nUser=root\nWorkingDirectory=/root\nStandardOutput=append:/var/log/monitoring.log\nStandardError=append:/var/log/monitoring.log\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/monitoring.service > /dev/null
+
 docker run -d \
   --name watchtower \
   -v /var/run/docker.sock:/var/run/docker.sock \
