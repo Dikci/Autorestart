81ddb95ee20fb469dc58f1e86118ca649da92b63
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-05-10 01:05:51 +0300

    Update gaianetrestart.sh

--- gaianetrestart.sh
+++ gaianetrestart.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-cd nwaku-compose
-docker compose down -v
-cd
-rm -rf nwaku-compose
-git clone https://github.com/waku-org/nwaku-compose
-cd nwaku-compose
-rm -rf keystore
-rm -rf docker-compose.yml
-wget https://raw.githubusercontent.com/Dikci/Autorestart/refs/heads/main/docker-compose.yml
-cp /root/.env /root/nwaku-compose/
-chmod +x register_rln.sh
-./register_rln.sh
-docker compose up -d
+# Найти и убить процессы на порту 8080
+PIDS=$(lsof -ti :8080)
+if [ -n "$PIDS" ]; then
+  echo "Killing process(es) on port 8080: $PIDS"
+  kill -9 $PIDS
+else
+  echo "No process found on port 8080."
+fi
+
+# Выполнить команды gaianet stop и start
+/root/gaianet/bin/gaianet stop
+/root/gaianet/bin/gaianet start
