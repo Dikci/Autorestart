ddb2e727f7550e9584bf94772dd788dfaf7547e0
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-08-22 11:26:35 +0300

    Update updating.sh

--- updating.sh
+++ updating.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+tmux kill-session -t nexus
+docker stop nexus && docker rm nexus && docker pull nexusxyz/nexus-cli:latest
+tmux new-session -d -s nexus bash -c 'bash -c "set -a; . /etc/environment; set +a; docker stop nexus; docker rm nexus; docker run -it --init --name nexus nexusxyz/nexus-cli:latest start --node-id \$ID; exec bash"
+
 npm install pm2 -g
 pm2 delete gensyn
 pm2 start /root/rl-swarm/run_rl_swarm.sh \
@@ -8,7 +13,3 @@ pm2 start /root/rl-swarm/run_rl_swarm.sh \
   --cwd /root/rl-swarm
 pm2 save
 pm2 logs gensyn
-
-tmux kill-session -t nexus
-docker stop nexus && docker rm nexus && docker pull nexusxyz/nexus-cli:latest
-tmux new-session -d -s nexus bash -c 'bash -c "set -a; . /etc/environment; set +a; docker stop nexus; docker rm nexus; docker run -it --init --name nexus nexusxyz/nexus-cli:latest start --node-id \$ID; exec bash"
