6f10990200876fe76dbcda01ed24a1c2b8f479f1
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-09-30 22:18:13 +0300

    Update gensyn.sh

--- gensyn.sh
+++ gensyn.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-mkdir -p backuppp 
-cp /root/rl-swarm/swarm.pem /root/backuppp
-rm -rf rl-swarm
-npm install -g yarn --force
-yarn install
-git clone https://github.com/gensyn-ai/rl-swarm
-cd rl-swarm
-python3 -m venv .venv && source .venv/bin/activate
-rm -rf run_rl_swarm.sh
-cp /root/backuppp/swarm.pem /root/rl-swarm/swarm.pem
-sed -i '$ a PORT=3999' /root/rl-swarm/modal-login/.env
-wget https://raw.githubusercontent.com/Dikci/Autorestart/refs/heads/main/run_rl_swarm.sh
-sed -i '/rm -r \$ROOT_DIR\/modal-login\/temp-data\/\*\.json/d' run_rl_swarm.sh 
-chmod +x run_rl_swarm.sh
-./run_rl_swarm.sh
+sed -i 's|http://localhost:3000|http://localhost:3999|g' $(grep -rl "http://localhost:3000" ~/rl-swarm)
+pm2 delete gensyn
+pm2 start /root/rl-swarm/run_rl_swarm.sh \
+  --name gensyn \
+  --interpreter bash \
+  --cwd /root/rl-swarm
+pm2 save
+pm2 logs gensyn
