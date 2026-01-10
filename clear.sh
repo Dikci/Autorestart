5b00e18973614e25ffa7fd011918793bdf5ecca6
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2026-01-10 18:42:18 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -7,7 +7,6 @@ docker volume prune -f          # автоответ "y"
 docker volume rm node-storage
 docker system prune -a -f       # автоответ "y"
 rm -rf multipleNEW.sh.1 multipleforlinux multiple.sh Ocean.sh.2 Ocean.sh.3 Ocean.sh.4 Ocean.sh.5 pop privasea .spheron
-rm -rf clear.sh
 
 
 rm -rf Monitoring.sh
