aef1ad124347049c7956d02383c6849093fa26db
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2026-01-10 18:38:48 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -9,6 +9,7 @@ docker system prune -a -f       # автоответ "y"
 rm -rf multipleNEW.sh.1 multipleforlinux multiple.sh Ocean.sh.2 Ocean.sh.3 Ocean.sh.4 Ocean.sh.5 pop privasea .spheron
 rm -rf clear.sh
 
+
 rm -rf Monitoring.sh
 rm -rf .aztec .cysic aztec aztec-sequencer aztec.sh aztecUP.sh backupG backup_rl_swarm backuppp bless cysicM.sh cysic-verifier gen gensyn.sh nexusU.sh nexusU.sh.1 nexusU.sh.2 rl-swarm rpc.py rpc.sh swarm.pem tg_role.sh titan.sh.1
 docker stop aztec-sequencer && docker rm aztec-sequencer
@@ -63,3 +64,38 @@ find / -type d \( -iname "*datagram*" -o -iname "*aztec*" -o -iname "*gensyn*" -
 
 echo "✅ DONE — NOTHING WILL RESTART"
 '
+
+rm -rf .drosera .drosera.db auto_irys auto_irys_test.sh drosera-backup drosera drosera-operator drosera-operator-v1.23.0-x86_64-unknown-linux-gnu.tar.gz drosera.sh.1 drosera.sh drosera_half.sh droseranew.sh
+
+docker stop unichain-node-op-node-1 unichain-node-execution-client-1
+docker rm unichain-node-op-node-1 unichain-node-execution-client-1
+sudo rm -r unichain-node
+
+sudo bash -c '
+SERVICES="auto_irys datagram gensyn irys-auto drosera"
+
+echo "[STOP & DISABLE]"
+for s in $SERVICES; do
+  systemctl stop $s.service 2>/dev/null
+  systemctl disable $s.service 2>/dev/null
+done
+
+echo "[REMOVE UNIT FILES]"
+rm -f /etc/systemd/system/auto_irys.service
+rm -f /etc/systemd/system/datagram.service
+rm -f /etc/systemd/system/gensyn.service
+rm -f /etc/systemd/system/irys-auto.service
+rm -f /lib/systemd/system/auto_irys.service
+rm -f /lib/systemd/system/datagram.service
+rm -f /lib/systemd/system/gensyn.service
+rm -f /lib/systemd/system/irys-auto.service
+rm -f /lib/systemd/system/drosera.service
+
+echo "[RELOAD SYSTEMD]"
+systemctl daemon-reexec
+systemctl daemon-reload
+
+echo "✅ SERVICES REMOVED"
+'
+
+
